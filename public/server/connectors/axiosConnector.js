9a5cf2928e7cb61f5402e333c379dd8c35267b34
LukaszW007 <https://LukaszW007@github.com>
2023-11-02 17:35:46 +0100

    Added public folder with server and client builds

---
+++ public/server/connectors/axiosConnector.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+export default class APIConnector {
+    static getJson(url, args) {
+        return axios
+            .get(url, {
+            // params: args,
+            responseType: "json",
+        })
+            .then((data) => {
+            // console.log('AXIOS response from : ',url,' ',data.data);
+            return data;
+        })
+            .catch((error) => {
+            console.log(error);
+        });
+    }
+}
---
+++ public/server/index.js
@@ -0,0 +1,81 @@
+import express from 'express';
+import APIConnector from './connectors/axiosConnector.js';
+import { requestLogger, unknownEndpoint } from './utils/middleware.js';
+import cors from 'cors';
+export const app = express();
+app.use(cors());
+app.use(requestLogger);
+app.use(express.json());
+// const PORT = process.env.PORT || 3001;
+const dataFetching = async () => {
+    let fetchedData = { stationInformation: null, stationInformationState: null, stationStatus: null, stationStatusState: null };
+    APIConnector.getJson('https://gbfs.urbansharing.com/oslobysykkel.no/station_information.json', null)
+        .then((data) => {
+        // console.log('data',data);
+        fetchedData.stationInformation = data.data.data.stations;
+        fetchedData.stationInformationState = {
+            last_updated: data.data.last_updated,
+            ttl: data.data.ttl,
+            version: data.data.version,
+        };
+        // fetchedData.stationInformationState = JSON.parse(data);
+    });
+    APIConnector.getJson('https://gbfs.urbansharing.com/oslobysykkel.no/station_status.json', null)
+        .then((data) => {
+        // console.log('data',data);
+        fetchedData.stationStatus = data.data.data.stations;
+        fetchedData.stationStatusState = {
+            last_updated: data.data.last_updated,
+            ttl: data.data.ttl,
+            version: data.data.version,
+        };
+    });
+    return fetchedData;
+};
+const fetchedAPIData = await dataFetching();
+app.get("/", (request, response) => {
+    response.send("<h1>Hello World!</h1>");
+});
+app.get("/api/station_information", (request, response) => {
+    response.json(fetchedAPIData.stationInformation);
+});
+app.get("/api/station_information/:id", (request, response) => {
+    const id = request.params.id;
+    let station;
+    if (fetchedAPIData.stationInformation) {
+        station = fetchedAPIData.stationInformation.find(st => st.station_id === id);
+    }
+    else {
+        console.log('Data is empty or not fetched from Oslo CityBike API');
+    }
+    if (station) {
+        response.json(station);
+    }
+    else {
+        response.status(404).end();
+    }
+});
+app.get("/api/station_status", (request, response) => {
+    response.json(fetchedAPIData.stationStatus);
+});
+app.get("/api/station_status/:id", (request, response) => {
+    const id = request.params.id;
+    let station;
+    if (fetchedAPIData.stationStatus) {
+        station = fetchedAPIData.stationStatus.find(st => st.station_id === id);
+    }
+    else {
+        console.log('Data is empty or not fetched from Oslo CityBike API');
+    }
+    if (station) {
+        response.json(station);
+    }
+    else {
+        response.status(404).end();
+    }
+});
+app.use(unknownEndpoint);
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
