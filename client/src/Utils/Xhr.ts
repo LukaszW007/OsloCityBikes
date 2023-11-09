18d00c63a1e155e37a909ebf6e65ba86c93d053a
LukaszW007 <https://LukaszW007@github.com>
2023-11-09 18:44:22 +0100

    #2 Reverse changes in XHR

--- client/src/Utils/Xhr.ts
+++ client/src/Utils/Xhr.ts
@@ -1,19 +1,18 @@
 import axios from 'axios';
 
 export class Xhr {
-    static async getJson(url: string, args: any) {
-        try {
-            const data = await axios.get(url, {
-                // params: args,
-                headers: {
-                    'Client-Identifier': 'my-company',
-                },
-                responseType: 'json',
-            });
-            console.log('AXIOS response from : ', url, ' ', data.data);
-            return data;
-        } catch (error) {
+    static getJson(url: string, args: any) {
+        return axios.get(url,{
+            // params: args,
+            headers: {
+                'Client-Identifier': 'my-company',
+            },
+            responseType: 'json',
+        }).then((data: any)=> {
+            console.log('AXIOS response from : ',url,' ',data.data);
+            return data
+        }).catch((error: any) => {
             console.log(error);
-        }
+        });
     }
 }
