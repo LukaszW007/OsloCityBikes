f2ef91f865d493ed47a28c8d7d8b9300c4038804
LukaszW007 <https://LukaszW007@github.com>
2023-11-12 18:31:03 +0100

    #1 Added more proper message to page404

--- client/src/Components/404.tsx
+++ client/src/Components/404.tsx
@@ -3,6 +3,9 @@ import React from "react"
 export const Page404 = () => {
 
   return (
-    <div className='flex flex-row justify-center text-5xl'>There is no such a page.</div>
+    <>
+    <div className='flex flex-row justify-center text-5xl'>There is no such a page</div>
+    <div className='flex flex-row justify-center text-1xl'>Choose one of the page number below</div>
+    </>
   )
 }
