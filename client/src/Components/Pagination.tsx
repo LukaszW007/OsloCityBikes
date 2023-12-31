960f448b5ffb65ee150dc1d5838e76eb9f4f96f2
Lukasz <lukasz.wiszniewski007@gmail.com>
2023-12-31 15:57:59 +0100

    fix(frontend) #8 entire size of buttons are clickable

--- client/src/Components/Pagination.tsx
+++ client/src/Components/Pagination.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 import { Link, generatePath } from "react-router-dom";
 
 interface PaginationProps {
-  currentPage: number,
-  itemsPerPage: number,
-  totalItems: number,
-  paginate: any,
+  currentPage: number;
+  itemsPerPage: number;
+  totalItems: number;
+  paginate: any;
 }
 export const Pagination = (props: PaginationProps) => {
   const { currentPage, itemsPerPage, totalItems, paginate } = props;
@@ -17,15 +17,20 @@ export const Pagination = (props: PaginationProps) => {
 
   return (
     <nav>
-      <ul className='flex justify-items-center flex-wrap justify-center'>
-      {pageNumbers.map((number: number) => (
-        <li key={number} className={currentPage === number ? "bg-custom-blue border-red-300 text-red-500 hover:bg-blue-200 relative inline-flex items-center px-4 py-2 border text-sm font-medium" : "bg-white border-gray-300 text-gray-500 hover:bg-blue-200 relative inline-flex items-center px-4 py-2 border text-sm font-medium"}>
-          {/* <Link to={`?page=${number}`} onClick={(e) =>paginate(number)}> */}
-          <Link to={`/${number}`} onClick={(e) =>paginate(number)}>
-						<span className='page-link'>
-							{number}
-						</span>
-						{/* <a onClick={(e) =>{
+      <ul className="flex justify-items-center flex-wrap justify-center">
+        {pageNumbers.map((number: number) => (
+          <li
+            key={number}
+            className={
+              currentPage === number
+                ? "bg-custom-blue border-red-300 text-red-500 hover:bg-blue-200 relative inline-flex items-center py-2 border text-sm font-medium"
+                : "bg-white border-gray-300 text-gray-500 hover:bg-blue-200 relative inline-flex items-center py-2 border text-sm font-medium"
+            }
+          >
+            {/* <Link to={`?page=${number}`} onClick={(e) =>paginate(number)}> */}
+            <Link to={`/${number}`} onClick={(e) => paginate(number)}>
+              <span className="page-link px-4 py-2">{number}</span>
+              {/* <a onClick={(e) =>{
 								paginate(number);
 								e.preventDefault();
 							}}
@@ -33,7 +38,7 @@ export const Pagination = (props: PaginationProps) => {
 						>
 							{number}
 						</a> */}
-					</Link>
+            </Link>
           </li>
         ))}
       </ul>
