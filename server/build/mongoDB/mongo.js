0419710fa4636d2f8101a15f3fb1b86cb6ba24c4
Lukasz <lukasz.wiszniewski007@gmail.com>
2024-12-05 19:34:16 +0100

    fix. Commented unnecessary code

--- server/build/mongoDB/mongo.js
+++ server/build/mongoDB/mongo.js
@@ -2,7 +2,8 @@
 import dotenv from "dotenv";
 dotenv.config();
 import mongoose from "mongoose";
-import { MongoClient, ServerApiVersion } from "mongodb";
+// import { MongoClient, ServerApiVersion } from "mongodb";
+// const { MongoClient, ServerApiVersion } = require("mongodb");
 // if (process.argv.length < 3) {
 // 	console.log("give password as argument");
 // 	process.exit(1);
@@ -26,29 +27,31 @@ const options = {
     serverSelectionTimeoutMS: 25000, // Increase the timeout to 25 seconds
 };
 ////////////////////////////////////////
-const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.wzqvkl2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    },
-});
-async function run() {
-    try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    }
-    finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
-    }
-}
-run().catch(console.dir);
+// const uri =
+// 	"mongodb+srv://<db_username>:<db_password>@cluster0.wzqvkl2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+// 	serverApi: {
+// 		version: ServerApiVersion.v1,
+// 		strict: true,
+// 		deprecationErrors: true,
+// 	},
+// });
+// async function run() {
+// 	try {
+// 		// Connect the client to the server	(optional starting in v4.7)
+// 		await client.connect();
+// 		// Send a ping to confirm a successful connection
+// 		await client.db("admin").command({ ping: 1 });
+// 		console.log(
+// 			"Pinged your deployment. You successfully connected to MongoDB!"
+// 		);
+// 	} finally {
+// 		// Ensures that the client will close when you finish/error
+// 		await client.close();
+// 	}
+// }
+// run().catch(console.dir);
 ////////////////////////////////////
 const connect = async () => {
     try {
