3c37739b23b3a7f3ecc7931d36404a2bc919dd92
Lukasz <lukasz.wiszniewski007@gmail.com>
2024-12-16 19:24:56 +0100

    fix. Assuming fetchedAPIData already exists

--- server/src/mongoDB/fetch-data.ts
+++ server/src/mongoDB/fetch-data.ts
@@ -12,29 +12,33 @@ let fetchedAPIData: FetchedAPIData;
 
 // Data fetching from API to update the map
 export const updateFromAPI = async () => {
+	await connect();
 	console.log("Starting data fetch...");
 	fetchedAPIData = await dataFetching();
 	console.log("Data is fetched");
+	await disconnect();
 };
 export const updateMongoDB = async () => {
 	// console.log("Starting data fetch...");
 	// const fetchedAPIData: FetchedAPIData = await dataFetching();
 	// console.log("Data is fetched");
 
-	let apiStatusData = null;
-	while (apiStatusData === null) {
-		await new Promise((resolve) => setTimeout(resolve, 500));
-		apiStatusData = fetchedAPIData.stationStatus;
-		console.log("status will be added to mongoDB");
-	}
-	await addApiStatusDataToStationsInfoCollection(apiStatusData!); //updating statuses collection
-	let apiData = null;
-	while (apiData === null) {
-		await new Promise((resolve) => setTimeout(resolve, 500));
-		apiData = fetchedAPIData.stationInformation;
-		console.log("stations will be added to mongoDB");
-	}
+	// let apiStatusData = null;
+	// while (apiStatusData === null) {
+	// 	await new Promise((resolve) => setTimeout(resolve, 500));
+	// 	apiStatusData = fetchedAPIData.stationStatus;
+	// 	console.log("status will be added to mongoDB");
+	// }
+	await addApiStatusDataToStationsInfoCollection(
+		fetchedAPIData.stationStatus!
+	); //updating statuses collection
+	// let apiData = null;
+	// while (apiData === null) {
+	// 	await new Promise((resolve) => setTimeout(resolve, 500));
+	// 	apiData = fetchedAPIData.stationInformation;
+	// 	console.log("stations will be added to mongoDB");
+	// }
 	// const apiData = fetchedAPIData.stationInformation;
-	await updateStationsCollection(apiData!); //updating stations' list collection
+	await updateStationsCollection(fetchedAPIData.stationInformation!); //updating stations' list collection
 	console.log("Data is fetching to update mongoDB");
 };
